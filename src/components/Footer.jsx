a2e7e664945a25465ea8d11275d6138c6e4f5f97
bryandanielpsp@gmail.com <bryandanielpsp@gmail.com>
2023-04-13 16:06:35 -0400

    React-counter-app

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+export default function Footer () {
+    const githubURL = "https://github.com/BryanVargas90/counter-app"
+
+return (
+    <footer>
+    <p><small>&copy; Bryan D. Vargas<br />
+     <a href={githubURL}  target="_blank" rel="noreferrer">see code in GitHub</a></small></p>
+
+   </footer>
+   
+    
+)
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header () {
+    return (
+        <main>
+        <h1>React Counter app </h1>
+        <p> React Web app studying useState() in React. </p>
+        </main>
+    )
+}
